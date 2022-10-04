ebf067953951f171189cbdce2e162588986207c1
Reman-tsega <99688221+Reman-tsega@users.noreply.github.com>
2022-10-04 15:38:45 +0300

    Time: 50 ms (54.04%), Space: 13.8 MB (96.60%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,14 +1,8 @@
 class Solution:
-    def removeLast(word):
-        return word.replace(word[-1],'')
-    
     def sortSentence(self, s: str) -> str:
         words=s.split()
         words.sort(key= lambda l:int(l[-1]))
-        
         removed_word= map(lambda word:word.replace(word[-1],''),words)
         sentence = " ".join(removed_word)
-        
-        
         return sentence
         
