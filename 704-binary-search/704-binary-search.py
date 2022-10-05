57a18eb8c6d9aeaa27014540700ee81b00b55d69
Reman-tsega <99688221+Reman-tsega@users.noreply.github.com>
2022-10-05 15:29:23 +0300

    Time: 631 ms (12.42%), Space: 15.5 MB (73.21%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -2,17 +2,15 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         nums.sort()
         lbi=0
-        ubi=len(nums)-1 #3
+        ubi=len(nums)-1 
         
         while(lbi<=ubi):
-            mid= lbi+(ubi-lbi)//2 #0
-            
-            # if target not in nums:
-            #     return -1
+            mid= lbi+(ubi-lbi)//2
+
             if nums[mid]==target:
                 return mid
             elif nums[mid]<target:
-                lbi= mid+1#1
+                lbi= mid+1
             elif nums[mid]> target:
-                ubi= mid-1 #0
+                ubi= mid-1 
         return -1
