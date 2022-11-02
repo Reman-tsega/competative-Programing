112b6d241c11bbd0086333319faca76433471f7a
Reman-tsega <99688221+Reman-tsega@users.noreply.github.com>
2022-11-02 06:20:38 +0300

    Time: 789 ms (24.25%), Space: 25.4 MB (29.57%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,20 +4,15 @@ class Solution:
         k = k % len(nums)
         count = 0
         start = 0
-        while count < len(nums):
-            current = start 
-            prev = nums[start] #store the value in the position
-            
+        while count<len(nums):
+            prev = nums[start]
+            current = start
             while True:
-                next = (current + k) % len(nums) #
-                temp = nums[next] #store it temporaly 
-                nums[next] = prev #overide the next 
-                prev = temp #reset prev
-                current = next #reset current
-                count += 1
-
-                if start == current:
-                    break 
-            
-            start += 1
-        
+                next = (k+current)%len(nums)
+                prev,nums[next]=nums[next], prev
+                count+=1
+                current = next
+                if start==current:
+                    break
+            start+=1
+                    
